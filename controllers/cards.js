7b4a4f25ed5bb32de61f0699abca8af8d2f94dcc
abramson21 <56224786+abramson21@users.noreply.github.com>
2020-01-13 23:18:26 +0300

    Add files via upload

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,20 +2,25 @@ const Card = require('../models/card');
 
 module.exports.getAllCards = (req, res) => {
   Card.find({})
-    .then(card => res.send({ data: card }))
-    .catch((error) => res.status(404).send({ message: error.message }));
-};
+    .then((card) => {
+      if (card.length === 0){
+        return res.status(404).send({ message: 'База данных cards пуста! '})
+      }
+      return res.send({ data: card })
+    })
+    .catch((error) => res.status(500).send({ message: error.message }));
+  }
 
 module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   const {name, link} = req.body;
   Card.create({name, link, owner})
     .then(card => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Не удается создать карточку' }));
+    .catch(() => res.status(500).send({ message: 'Не удается создать карточку' }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
       .then(user => res.send({ data: user }))
-      .catch(() => res.status(404).send({ message: 'Данной карточки нет' }));
+      .catch(() => res.status(500).send({ message: 'Данной карточки нет' }));
 }
--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,20 @@ const User = require('../models/user');
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
-  .then(user => res.send({ data: user }))
-  .catch((error) => res.status(404).send({ message: error.message }));
+  .then((user) => {
+    if (user.length === 0){
+      return res.status(404).send({ message: 'База данных user пуста! '})
+    }
+    return res.send({ data: user })
+  })
+  .catch((error) => res.status(500).send({ message: error.message }));
 };
 
 module.exports.createUser = (req, res) => {
   const {name, about, avatar} = req.body;
   User.create({name, about, avatar})
     .then(user => res.send({ data: user }))
-    .catch(() => res.status(404).send({ message: 'Не удалось создать пользователя' }));
+    .catch(() => res.status(500).send({ message: 'Не удалось создать пользователя' }));
 };
 
 module.exports.getUser = (req, res) => {
@@ -22,5 +27,5 @@ module.exports.getUser = (req, res) => {
       res.send({ userId });
     }
   })
-  .catch(() => res.status(404).send({ message: "Нет пользователя с таким id"}));
+  .catch(() => res.status(500).send({ message: "Нет пользователя с таким id"}));
 };
